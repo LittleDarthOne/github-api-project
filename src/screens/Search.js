b2f931376979678941eb6a8d1b802abb80fa297e
Jeferson de Oliveira <jef.oli@gmail.com>
2019-07-20 11:51:28 -0300

    Repo's star component and a fake repos list on search

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, FlatList } from 'react-native';
 
+import Text      from '../components/Text';
 import SearchBox from '../components/SearchBox';
 import Card      from '../components/Card';
 import RepoInfo  from '../components/RepoInfo';
@@ -8,44 +9,55 @@ import Api       from '../services/Api';
 
 export default class Search extends Component {
   state = {
-    searchText: undefined,
+    repos: [],
     response:'',
   };
 
   handleSearch = async (searchText) => {
-    this.setState({ searchText });
     const response = await Api.get(`/users/${searchText}` );
     console.log(response);
+    this.setState({ repos: [...Array(10)] });
   };
 
-  
+  renderEmptyMessage = () => {
+    return(
+      <Text style={styles.emptyMessage}>Nenhum repositório encontrado</Text>
+    );
+  };
+
+  extractRepoKey = (repo, index) => `repo-${index}`;
+
+  renderRepoCard = ({ item, index }) => {
+    const { navigation } = this.props;
+
+    const name = `Fake ${(index + 1)}`;
+    const description = `Descrição do repositório ${name}`;
+    const stars = index * 10;
+
+    return(
+      <Card style={styles.card} onPress={() => navigation.navigate('Detail')}>
+        <RepoInfo 
+          userAvatar="https://facebook.github.io/react-native/docs/assets/favicon.png" 
+          name={name}
+          description={description}
+          stars={stars}
+        />
+      </Card>
+    );
+  };
 
   render() {
-    const { searchText } = this.state;
-    const { navigation } = this.props;
+    const { repos } = this.state;
 
     return (
       <View style={styles.container}>
         <SearchBox onSearch={this.handleSearch} />
-
-        <Card style={styles.card} onPress={() => navigation.navigate('Detail')}>
-          <RepoInfo 
-            userAvatar="https://facebook.github.io/react-native/docs/assets/favicon.png" 
-            name={searchText}
-            description={`Descrição do repo ${searchText}`}
-          />
-        </Card>
-
-        <Card style={styles.card}>
-          <RepoInfo 
-            name={searchText}
-            description={`Descrição do repo ${searchText}`}
-          />
-        </Card>
-
-        <Card style={styles.card}>
-          <RepoInfo />
-        </Card>
+        <FlatList 
+          data={repos}
+          renderItem={this.renderRepoCard}
+          keyExtractor={this.extractRepoKey}
+          ListEmptyComponent={this.renderEmptyMessage}
+        />
       </View>
     );
   }
@@ -62,4 +74,10 @@ const styles = StyleSheet.create({
   card: {
     marginTop: 24,
   },
+
+  emptyMessage: {
+    textAlign: 'center',
+    fontStyle: 'italic',
+    margin: 24,
+  }
 });
